9efe7ae5d3fd822791e0e3918534727974d66c1e
nateenatea <52230308+nateenatea@users.noreply.github.com>
2020-08-29 23:01:17 +0700

    Update by natee

---
+++ exercise/vending_machine/build/precache-manifest.f82e29dbbeee946578b64c0cb309089c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f314d19816b44088446319fb081d2730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c91aac539eff7808da",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "aefbf37bbed68d5e2e4f",
+    "url": "/static/css/main.eb6dcc01.chunk.css"
+  },
+  {
+    "revision": "85c91aac539eff7808da",
+    "url": "/static/js/2.dd57285f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.dd57285f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefbf37bbed68d5e2e4f",
+    "url": "/static/js/main.47429b28.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
